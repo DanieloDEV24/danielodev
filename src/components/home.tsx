73c10ac5c4a17986fa1f4f9eda231500218857ea
DanieloDEV24 <danielruizdeveloper@gmail.com>
2026-08-26 00:03:50 +0200

    mejora semantica

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,37 +1,37 @@
-import { CardHome } from "./cardDev"
 
 export const Home = () => {
     return(
-        <div id="home" style={{ position: 'relative' }}>
+        <section id="home" aria-labelledby="titulo-home">
 
             <div id="contenedor-home">
-                <div className="contenedor-titulo">
-                    <h1 className='titulo-home-dev'>DESARROLLADOR</h1>
-                    <h1 className='titulo-home-full'>FULL STACK</h1>
-                </div>
+                
+                <h1>
+                    <span className='titulo-home-dev'>DESARROLLADOR</span>
+                    <span className='titulo-home-full'>FULL STACK</span>
+                </h1>
 
                 <p className="texto-home">
                     Soy Daniel, desarrollador Full Stack. Me gusta construir cosas que funcionen y que la gente use de verdad. Aprendo rápido, me adapto a lo que el proyecto necesita y siempre busco mejorar.
                 </p>
 
-                <div className="contenedor-contadores-home">
+                <dl className="contenedor-contadores-home">
                     <div>
-                        <h1>+3</h1>
-                        <p>proyectos Reales</p>
+                        <dt>+3</dt>
+                        <dd>proyectos Reales</dd>
                     </div>
 
                     <div>
-                        <h1>+8</h1>
-                        <p>herramientas</p>  
+                        <dt>+8</dt>
+                        <dd>herramientas</dd>  
                     </div>
 
                     <div>
-                        <h1>+1</h1>
-                        <p>año de experiencia</p>
+                        <dt>+1</dt>
+                        <dd>año de experiencia</dd>
                     </div>
-                </div>
+                </dl>
             </div>
 
-        </div>
+        </section>
     )
 }
