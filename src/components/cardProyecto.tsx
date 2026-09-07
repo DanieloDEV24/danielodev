184309871ec3081b99dae204c366a875870bd8a2
DanieloDEV24 <danielruizdeveloper@gmail.com>
2026-09-07 18:18:15 +0200

    cards proyectos en desarrollo

--- src/components/cardProyecto.tsx
+++ src/components/cardProyecto.tsx
@@ -2,7 +2,18 @@ export const CardProyecto = ({img, altImg, titulo, descripcion, estado, srcGitHu
     return (
         <article className="card-proyecto">
             <img src={img} alt={altImg} />
-            <h3 className="title-proyecto">{titulo}.</h3>
+            <div className="title-develop">
+                <h3 className="title-proyecto">{titulo}.</h3>
+                <small className={`estado-proyecto ${!estado ? 'desarrollo' : 'finalizado'}`}>
+                    <span className="icono"></span>
+                    <span className="texto">En desarrollo <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-code">
+	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+	<path d="M7 8l-4 4l4 4" />
+	<path d="M17 8l4 4l-4 4" />
+	<path d="M14 4l-4 16" />
+</svg></span>
+                </small>
+            </div>
             <p className="descripcion-proyecto">{descripcion}</p>
             <ul className="links">
                 <li><a href={srcGitHub} target="_blank" rel="noopener noreferrer">
